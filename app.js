21b7b069f108db87d6c71fc5dba599f2ebdff9b9
jeret <jeretgamer444@gmail.com>
2025-09-04 03:38:10 -0300

    Listo para Render

--- app.js
+++ app.js
@@ -3,34 +3,33 @@ const app = express()
 const http = require('http'); 
 const routerusuarios = require("./routers/usuarios")
 const routerclientes = require("./routers/clientes")
-const server = http.createServer(app);   
-const port = 3000;
+const server = http.createServer(app);   
+const port = process.env.PORT || 3000;
 const path = require('path');
 const session = require("express-session");
 
 app.use(
-  session({
-    secret: "mi_secreto_seguro", // Cambia esto por algo único
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }, // Cambiar a true si usas HTTPS
-  })
+  session({
+    secret: "mi_secreto_seguro", 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, 
+  })
 );
 
 
+// Configuración de body-parser  
+app.use(express.urlencoded({ extended: false }));  
+app.use(express.json());  
+// Archivos estáticos  
+app.use('/public', express.static("public"));  
+app.use(express.static(path.join(__dirname, 'public')));  
 
-// Configuración de body-parser  
-app.use(express.urlencoded({ extended: false }));  
-app.use(express.json());  
-// Archivos estáticos  
-app.use('/public', express.static("public"));  
-app.use(express.static(path.join(__dirname, 'public')));  
-
-// Configuración de EJS como motor de vistas  
-app.set("view engine", "ejs");  
+// Configuración de EJS como motor de vistas  
+app.set("view engine", "ejs");  
 app.use("/", routerusuarios); 
 app.use("/", routerclientes);
 
-server.listen(port, () => {  
-    console.log(`Servidor corriendo en http://localhost:${port}`);  
+server.listen(port, () => {  
+    console.log(`Servidor corriendo en el puerto ${port}`);  
 });
